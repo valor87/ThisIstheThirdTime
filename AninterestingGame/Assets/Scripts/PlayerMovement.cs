bbaa26468ad19ffb39b68dcf0e2722139833e493
Anthony-Pattison <pattisoa@sheridancollege.ca>
2025-04-19 14:12:19 -0400

    Addig a new font, cinimachine and changing some of the player code

--- AninterestingGame/Assets/Scripts/PlayerMovement.cs
+++ AninterestingGame/Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5;
     public bool canMove = true;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+   
     // Update is called once per frame
     void Update()
     {
@@ -19,5 +14,15 @@ public class PlayerMovement : MonoBehaviour
             transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
             transform.position += Vector3.up * Input.GetAxis("Vertical") * speed * Time.deltaTime;
         }
+
+        if (Input.GetKey("left shift"))
+        {
+            speed = 7;
+        }
+        else
+        {
+            speed = 5;
+        }
+
         }
 }
