2d2b839224cc122e73b2b91863ccc117ab11d3dd
Anthony-Pattison <pattisoa@sheridancollege.ca>
2025-04-29 19:50:16 -0400

    Putting the idle animation in, giving the animation to the character, changes animation through code

--- AninterestingGame/Assets/Scripts/PlayerMovement.cs
+++ AninterestingGame/Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.XR;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
+    bool idleright;
+    bool idleleft;
+    bool idledown;
+    bool idleup;
+
     public float speed = 5;
     public bool canMove = true;
-   
+
+    public Animator PA;
     // Update is called once per frame
     void Update()
     {
-        if (canMove) {
+        PA.SetBool("IdleLeft", idleleft);
+        PA.SetBool("IdleRight", idleright);
+        PA.SetBool("IdleUp", idleup);
+        PA.SetBool("IdleDown", idledown);
+
+        if (canMove)
+        {
             transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
             transform.position += Vector3.up * Input.GetAxis("Vertical") * speed * Time.deltaTime;
         }
+
+        changeanimation();
 
         if (Input.GetKey("left shift"))
         {
@@ -24,5 +40,42 @@ public class PlayerMovement : MonoBehaviour
             speed = 5;
         }
 
+    }
+    bool idlechange(int i)
+    {
+        if (i == 1)
+        {
+            return true;
         }
+        else
+        {
+            return false;
+        }
+    }
+    private void changeanimation()
+    {
+        if (Input.GetKeyDown("d"))
+        {
+            idleright = idlechange(1);
+        }
+        else if (Input.GetKeyDown("w"))
+        {
+            idleup = idlechange(1);
+        }
+        else if (Input.GetKeyDown("a"))
+        {
+            idleleft = idlechange(1);
+        }
+        else if (Input.GetKeyDown("s"))
+        {
+            idledown = idlechange(1);
+        }
+        else
+        {
+            idleup = idlechange(0); idledown = idlechange(0); idleleft = idlechange(0); idleright = idlechange(0);
+        }
+    }
+
 }
+
+
