db0d6125baef1a154ce7408178633d90507d0cde
Anthony-Pattison <pattisoa@sheridancollege.ca>
2025-05-02 23:29:13 -0400

    imported the walk cycle for esther, the movement and the playing of the animation is very primitive and needs imporvement

--- AninterestingGame/Assets/Scripts/PlayerMovement.cs
+++ AninterestingGame/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEditor.XR;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -11,29 +12,35 @@ public class PlayerMovement : MonoBehaviour
     public bool idledown;
     public bool idleup;
 
-    public float speed = 5;
+    public float speed;
     public bool canMove = true;
     Rigidbody2D Rb;
-    
+    Vector2 tempx;
+    Vector2 tempy;
     public Animator PA;
-     void Start()
+    public float speedani;
+    public float speedani2;
+    void Start()
     {
-    
+        Rb = GetComponent<Rigidbody2D>();
+         
     }
     // Update is called once per frame
     void Update()
     {
-        
+
+        //Debug.Log(directionx);
         
         if (canMove)
         {
-            PA.SetBool("IdleLeft", idleleft);
-            PA.SetBool("IdleRight", idleright);
-            PA.SetBool("IdleUp", idleup);
+            
+         
             PA.SetBool("IdleDown", idledown);
 
             transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
             transform.position += Vector3.up * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+           
+           
         }
 
         changeanimation();
@@ -44,7 +51,7 @@ public class PlayerMovement : MonoBehaviour
         }
         else
         {
-            speed = 5;
+            //speed = 5;
         }
 
     }
@@ -61,26 +68,38 @@ public class PlayerMovement : MonoBehaviour
     }
     private void changeanimation()
     {
-        if (Input.GetKeyDown("d"))
+       
+        if (Input.GetKey("d"))
         {
             idleright = idlechange(1);
+            speedani = 1;
+            
         }
-        else if (Input.GetKeyDown("w"))
+        else if (Input.GetKey("w"))
         {
             idleup = idlechange(1);
+            speedani2 = 1;
         }
-        else if (Input.GetKeyDown("a"))
+        else if (Input.GetKey("a"))
         {
             idleleft = idlechange(1);
+            speedani = -1;
         }
-        else if (Input.GetKeyDown("s"))
+    
+        else if (Input.GetKey("s"))
         {
             idledown = idlechange(1);
+            speedani2 = -1;
         }
         else
         {
-           idleup = idlechange(0); idledown = idlechange(0); idleleft = idlechange(0); idleright = idlechange(0);
+            speedani = 0;
+            speedani2 = 0;
+            idleup = idlechange(0); idledown = idlechange(0); idleleft = idlechange(0); idleright = idlechange(0);
         }
+        Debug.Log(speedani);
+        PA.SetFloat("Directionx", speedani);
+        PA.SetFloat("Directiony", speedani2);
     }
 
 }
