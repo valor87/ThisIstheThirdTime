350d9a1b56dc7558cb25e7333c82826bb8cbacd2
Anthony-Pattison <pattisoa@sheridancollege.ca>
2025-05-15 21:27:15 -0400

    Improving the needle game and adding velocity to the car mini as well as a brake

--- AninterestingGame/Assets/Scripts/Neeble.cs
+++ AninterestingGame/Assets/Scripts/Neeble.cs
@@ -4,26 +4,55 @@ using UnityEngine;
 
 public class Neeble : MonoBehaviour
 {
-    public bool ison1 = true;
+    bool ison1 = true;
     bool ison2 = true;
     bool ison3 = true;
     Vector3 needleTransform;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         
     }
-
+    public void SliderValuechange(float turn)
+    {
+        needleTransform = transform.eulerAngles;
+        transform.eulerAngles = needleTransform - new Vector3(0,0, turn);
+    }
+   
+    IEnumerator Yabadaba (float degrees, bool isonTRUE)
+    {
+        Vector3 rotation = transform.eulerAngles;
+        
+        if (isonTRUE)
+        {
+            int temp = 0;
+            while (temp != degrees)
+            {
+                temp++;
+                rotation = transform.eulerAngles;
+                transform.eulerAngles += Vector3.back;
+                yield return new WaitForSeconds(0.02f);
+                isonTRUE = false;
+            }
+        }
+        else
+        {
+            int temp = 0;
+            while (temp != degrees)
+            {
+                temp++;
+                rotation = transform.eulerAngles;
+                transform.eulerAngles -= Vector3.back;
+                yield return new WaitForSeconds(0.02f);
+                isonTRUE = true;
+            }
+        }
+    }
     public void ButtonPressed()
     {
         StartCoroutine(Yabadaba(90, ison1));
-        if (ison1) {
+        if (ison1)
+        {
             ison1 = false;
         }
         else
@@ -53,35 +82,6 @@ public class Neeble : MonoBehaviour
         else
         {
             ison3 = true;
-        }
-    }
-    IEnumerator Yabadaba (float degrees, bool isonTRUE)
-    {
-        Vector3 rotation = transform.eulerAngles;
-        
-        if (isonTRUE)
-        {
-            int temp = 0;
-            while (temp != degrees)
-            {
-                temp++;
-                rotation = transform.eulerAngles;
-                transform.eulerAngles += Vector3.back;
-                yield return new WaitForSeconds(0.02f);
-                isonTRUE = false;
-            }
-        }
-        else
-        {
-            int temp = 0;
-            while (temp != degrees)
-            {
-                temp++;
-                rotation = transform.eulerAngles;
-                transform.eulerAngles -= Vector3.back;
-                yield return new WaitForSeconds(0.02f);
-                isonTRUE = true;
-            }
         }
     }
 }
--- AninterestingGame/Assets/Scripts/UIcarmovement.cs
+++ AninterestingGame/Assets/Scripts/UIcarmovement.cs
@@ -9,6 +9,9 @@ public class UIcarmovement : MonoBehaviour
     float yspeed;
     float xspeed;
 
+    float tempy;
+    float tempx;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,25 +21,33 @@ public class UIcarmovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        xspeed += tempx;
+        yspeed += tempy;
     transform.position += new Vector3(xspeed, yspeed, 0);
         
     }
 
     public void MoveRight()
     {
-       
+        tempx += 0.001f;
     }
     public void MoveLeft()
     {
-
+        tempx -= 0.001f;
     }
     public void MoveUp()
     {
-
+        tempy += 0.001f;
     }
     public void MoveDown()
     {
-
+        tempy -= 0.001f;
+    }
+    public void Break()
+    {
+        tempy = 0;
+        tempx = 0;
+        xspeed = 0;
+        yspeed = 0;
     }
 }
