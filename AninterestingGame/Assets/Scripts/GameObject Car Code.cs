de9bcb86fdf09e15560f13b4884951f1e2d0c264
Anthony-Pattison <pattisoa@sheridancollege.ca>
2025-05-30 21:51:42 -0400

    The car stops on its own and starts again when the mouse clicks it

--- AninterestingGame/Assets/Scripts/GameObject Car Code.cs
+++ AninterestingGame/Assets/Scripts/GameObject Car Code.cs
@@ -15,19 +15,22 @@ public class GameObjectCarCode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 pos = transform.position;
         Vector2 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-       // Debug.Log(mousepos);
+
         if (Input.GetMouseButton(1) && GetComponent<SpriteRenderer>().bounds.Contains(mousepos))
         {
-            GetComponent<SplineAnimate>().enabled = false;
-            transform.position = mousepos;
-
+            GetComponent<SplineAnimate>().ElapsedTime += 0.8f;
         }
         else
         {
             GetComponent<SplineAnimate>().enabled = true;
             GetComponent<SplineAnimate>().Play();
-           // GetComponent<SplineAnimate>().MaxSpeed = 5f;
+        }
+
+        if (new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y)) == new Vector2(1, -2))
+        {
+            GetComponent<SplineAnimate>().Pause();
         }
     }
 }
