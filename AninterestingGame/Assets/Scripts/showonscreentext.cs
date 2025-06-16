48f0b76e0424db684cfee95229e0417b81f92ffb
Anthony-Pattison <pattisoa@sheridancollege.ca>
2025-06-15 20:23:47 -0400

    The text box is now self contained and does not need a manager with it

--- AninterestingGame/Assets/Scripts/showonscreentext.cs
+++ AninterestingGame/Assets/Scripts/showonscreentext.cs
@@ -6,19 +6,31 @@ using UnityEngine;
 
 public class showonscreentext : MonoBehaviour
 {
-    public GameObject dialaugeCanvus;
-    public TextMeshProUGUI text;
+    GameObject Image;
+    GameObject conIcon;
+    GameObject Textobject;
+    TextMeshProUGUI text;
     public TextAsset textasset;
     public string[] dialauge;
     public float textSpeed = 0.05f;
     bool continueText;
+    private void Start()
+    {
+        Transform imagetr = transform.GetChild(0);
+        Image = imagetr.gameObject;
+        Transform texttr = transform.GetChild(1);
+        Textobject = texttr.gameObject;
+        text = Textobject.GetComponent<TextMeshProUGUI>();
+        Transform contr = transform.GetChild(2);
+        conIcon = contr.gameObject;
+    }
     // Update is called once per frame
     void Update()
     {
-       
+
         if (Input.GetKeyDown("space"))
         {
-            dialaugeCanvus.SetActive(true); // turns on the canvus
+            turnOnUi(); // turns on the canvus
             dialauge = (textasset.text.Split('\n')); // splits the entire text doc by when ever the enter button is pressed
 
             StartCoroutine(slowtext()); // starts the coroutine
@@ -26,7 +38,7 @@ public class showonscreentext : MonoBehaviour
     }
     IEnumerator slowtext()
     {
-        
+
         for (int i = 0; i < dialauge.Count(); i++)
         {
             string currenttext = dialauge[i]; // sets the line of the txt file
@@ -37,24 +49,38 @@ public class showonscreentext : MonoBehaviour
                 Debug.Log("Working");
                 yield return new WaitForSeconds(textSpeed); // waits based on the text speed
             }
-            
+
             yield return new WaitForSeconds(1);
-           
+
             continueText = true;
 
             while (continueText)
             {
-                
+
                 if (Input.GetKey("w"))
                 {
                     continueText = false;
+                    text.text = string.Empty; // at the end of all the lines it sets the text box to empty
                 }
                 yield return null;
             }
             text.text = string.Empty; // at the end of all the lines it sets the text box to empty
         }
-        dialaugeCanvus.SetActive(false);// turns off the canvus
+        turnOffUi();// turns off the canvus
         yield return null;
 
+    }
+
+    private void turnOnUi()
+    {
+        Image.SetActive(true);
+        Textobject.SetActive(true);
+        conIcon.SetActive(true);
+    }
+    private void turnOffUi()
+    {
+        Image.SetActive(false);
+        Textobject.SetActive(false);
+        conIcon.SetActive(false);
     }
 }
