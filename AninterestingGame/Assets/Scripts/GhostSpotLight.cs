509c1cc051e6394867c50dc2b41fe6c5101cfda6
Anthony-Pattison <pattisoa@sheridancollege.ca>
2025-07-21 22:58:41 -0400

    messing around with the ghosts

--- AninterestingGame/Assets/Scripts/GhostSpotLight.cs
+++ AninterestingGame/Assets/Scripts/GhostSpotLight.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class GhostSpotLight : MonoBehaviour
 {
     public GameObject Player;
-
+    public GameObject SpotGuage;
+    SpriteRenderer sp;
+    Vector2 guagepos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sp = GetComponent<SpriteRenderer>();
+        guagepos = Camera.main.ScreenToWorldPoint(SpotGuage.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        Debug.Log(guagepos);
+        SpotGuage.transform.position = Player.transform.position;
+        if (sp.bounds.Contains(Player.transform.position))
+        {
+            GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = Color.red;
+        }
+
     }
 
 }
