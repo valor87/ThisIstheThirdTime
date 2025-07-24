8de131b30a0572ee279256610c7160133d40f887
Anthony-Pattison <pattisoa@sheridancollege.ca>
2025-07-23 23:11:41 -0400

    Saving scene, and the ghost visual bar is done just need to change the slider with the animation

--- AninterestingGame/Assets/Scripts/GhostSpotLight.cs
+++ AninterestingGame/Assets/Scripts/GhostSpotLight.cs
@@ -7,17 +7,32 @@ public class GhostSpotLight : MonoBehaviour
 {
     public GameObject slider;
     public float visionmeter;
+    public bool adding;
+    private void Update()
+    {
+        if (visionmeter > 0 && !adding)
+        {
+
+            visionmeter -= 5 * Time.deltaTime;
+
+        }
+        slider.GetComponent<Slider>().value = visionmeter;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-            Debug.Log("collision");
+        Debug.Log("collision");
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
+        adding = true;
         Debug.Log("staying collision");
-        visionmeter += 0.1f;
-        slider.GetComponent<Slider>().value = visionmeter;
+        visionmeter += 5 * Time.deltaTime;
+        
     }
-
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        adding = false;
+    }
     public void showint(float temp)
     {
         temp = visionmeter;
