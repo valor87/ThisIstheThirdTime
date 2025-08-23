af2f086dfeb8ade6e813f96ea4419f6b69919c3d
Anthony-Pattison <pattisoa@sheridancollege.ca>
2025-08-23 01:20:14 -0400

    Adding in some sprites and laying out the hallway. the ghosts also make esther respawn at the begining of the basement

--- AninterestingGame/Assets/Scripts/GhostVisonManager.cs
+++ AninterestingGame/Assets/Scripts/GhostVisonManager.cs
@@ -8,14 +8,49 @@ public class GhostVisonManager : MonoBehaviour
     public float visionmetermanager;
     public bool adding;
     public GameObject slider;
-    
+    public GameObject Esther;
+
+    public GameObject MainCameraFadeBox;
+    float fadeamount;
+    public bool isrunning;
+    public bool changed;
     // Update is called once per frame
     void Update()
     {
-       
+        if (visionmetermanager >= 20)
+        {
+            StartCoroutine(CameraFade());
+            
+            visionmetermanager = 0;
+        }
     }
     public void showint(float temp)
     {
         temp = visionmetermanager;
+    }
+
+    private IEnumerator CameraFade()
+    {
+
+        isrunning = true; // runs the code only onces
+        Esther.GetComponent<PlayerMovement>().canMove = false; // stop the player from moving
+        while (fadeamount < 3)
+        {
+            fadeamount += 0.03f; // increase the transparancy of the box infrount of the camera
+            MainCameraFadeBox.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, fadeamount); // give the color
+            yield return null;
+        }
+
+        Esther.transform.position = new Vector2(-4.46f, -1.77f);
+        changed = true;
+        while (fadeamount > 0)
+        {
+            fadeamount -= 0.03f; // decrease the transparancy of the box infrount of the camera
+            MainCameraFadeBox.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, fadeamount); // give the color
+            yield return null;
+        }
+        Esther.GetComponent<PlayerMovement>().canMove = true; // give movement back
+        isrunning = false; // set up so it can run again
+        changed = false;
     }
 }
