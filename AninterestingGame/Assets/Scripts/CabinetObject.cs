ca619f73e66ba8cb796020a98b599d1416b40546
Anthony-Pattison <pattisoa@sheridancollege.ca>
2025-09-20 15:29:59 -0400

    Adding art to the basement scene and laying out a basement test scene

--- AninterestingGame/Assets/Scripts/CabinetObject.cs
+++ AninterestingGame/Assets/Scripts/CabinetObject.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class CabinetObject : MonoBehaviour
 {
     public Transform esthertransform;
     public Transform othercloset;
-    public List<GameObject> ignoreobject;
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (Input.GetKey("space") && esthertransform.position == collision.gameObject.transform.position)
         {
-         collision.transform.position = othercloset.position - new Vector3(1,0,0);
+            collision.transform.position = othercloset.position - new Vector3(0, 1.5f, 0);
         }
+       
     }
 }
